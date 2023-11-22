3ff3cb1d1071650038faac74e0817dfc40e459f9
markrenssi <mark.renssi@aalto.fi>
2023-11-22 02:21:33 +0200

    Fixed eslint errors

--- test/PokemonPage.jest.spec.jsx
+++ test/PokemonPage.jest.spec.jsx
@@ -26,7 +26,7 @@ const pokemonList = {
     {
       ability: {
         name: 'anticipation',
-        url: 'https://pokeapi.co/api/v2/ability/107/' // eslint-disable-line no-console
+        url: 'https://pokeapi.co/api/v2/ability/107/' // eslint-disable-line
       },
       is_hidden: true,
       slot: 3
